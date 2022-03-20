64864511af839a39906e27337f513789dc7f21f9
Astro3000 <94796723+Astro3000x@users.noreply.github.com>
2022-03-20 16:07:28 +0000

    Now has a submission that will still be updated in the future.

--- sharepage.js
+++ sharepage.js
@@ -1,3 +1,16 @@
+function submit() {
+    var currentdate = new Date();
+
+    
+    var pname = document.getElementById("pname").value;
+    
+    var uname = document.getElementById("uname").value;
+    
+    var code = document.getElementById("txt").value;
+    
+    alert(pname + uname + code);
+    window.open('mailto:a3animationsofficial123@gmail.com?subject=CodeSubmission&body=ProjectName: ' + pname + ' UserName: ' + uname + ' Code: ' + code + ' Time: ' + currentdate);
+}
 function run1() {
     var code = document.getElementById("code1").value;
     
